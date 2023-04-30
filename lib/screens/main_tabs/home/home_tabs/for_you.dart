9eeb543f4b4e80f2cac3acbc51a7253859a9b0a9
Akshit Mahaur <mahaurakshit7@gmail.com>
2023-05-01 02:32:30 +0530

    feat: pull to refresh added

--- lib/screens/main_tabs/home/home_tabs/for_you.dart
+++ lib/screens/main_tabs/home/home_tabs/for_you.dart
@@ -2,56 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:twitter_ui/models/post.dart';
 import 'package:twitter_ui/services/mongoConnect.dart';
 
-class ForYou extends StatelessWidget {
+class ForYou extends StatefulWidget {
   const ForYou({super.key});
 
   @override
+  State<ForYou> createState() => _ForYouState();
+}
+
+class _ForYouState extends State<ForYou> {
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: MongoDatabase.getDocuments(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Container(
-            color: Colors.white,
-            child: const LinearProgressIndicator(
-              backgroundColor: Colors.black,
-            ),
-          );
-        } else {
-          if (snapshot.hasError) {
+    return RefreshIndicator(
+      onRefresh: () {
+        return Future.delayed(
+          const Duration(seconds: 1),
+          () {
+            setState(() {
+              // ignore: avoid_print
+              print('Refreshed');
+            });
+            
+          },
+        );
+      },
+      child: FutureBuilder(
+        future: MongoDatabase.getDocuments(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return Container(
               color: Colors.white,
-              child: const Center(
-                child: Text(
-                  'Something went wrong, try again.',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                  )
-                ),
+              child: const LinearProgressIndicator(
+                backgroundColor: Colors.black,
               ),
             );
           } else {
-            return ListView.builder(
-                itemCount: snapshot.data?.length,
-                itemBuilder: (context, index) => Card(
-                      child: Column(
-                        children: [
-                          ListTile(
-                            leading: const CircleAvatar(
-                              child: FlutterLogo(),
+            if (snapshot.hasError) {
+              return Container(
+                color: Colors.white,
+                child: const Center(
+                  child: Text('Something went wrong, try again.',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                      )),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) => Card(
+                        child: Column(
+                          children: [
+                            ListTile(
+                              leading: const CircleAvatar(
+                                child: FlutterLogo(),
+                              ),
+                              title: Text('User $index'),
+                              subtitle: Text('Tweet $index'),
                             ),
-                            title: Text('User $index'),
-                            subtitle: Text('Tweet $index'),
-                          ),
-                          Image.network(snapshot.data![index]['content'] ?? ''),
-                          // Image.network(Posts.fromJson(snapshot.data![index]).content ?? ''),
-                        ],
-                      ),
-                    ));
+                            Image.network(
+                                snapshot.data![index]['content'] ?? ''),
+                            // Image.network(Posts.fromJson(snapshot.data![index]).content ?? ''),
+                          ],
+                        ),
+                      ));
+            }
           }
-        }
-      },
+        },
+      ),
     );
   }
 }
